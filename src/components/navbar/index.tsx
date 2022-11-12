ec0dc552fd148f5d74fdb8227eb3070d36e80f57
Mateus Pragana <mateuspragana@gmail.com>
2022-11-12 18:11:48 -0300

    Trabalhando na responsividade do sidebar.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,25 +4,39 @@ import { NavLink } from "react-router-dom"
 import "twin.macro"
 
 const NavBar: React.FC = () => {
+
+
     return (
-        <aside tw="w-64">
-            <div tw="overflow-y-auto h-screen py-4 px-3 rounded-r bg-gray-800">
-                <ul tw="space-y-2">
-                    <li>
-                        <NavLink to="/" tw="[&.active]:bg-gray-600 cursor-pointer flex items-center p-2 text-base font-normal  rounded-lg text-white hover:bg-gray-700">
-                            <ChartBar size={32} />
-                            <span tw="ml-3">Dashboard</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/admin" tw="[&.active]:bg-gray-600 cursor-pointer flex items-center p-2 text-base font-normal rounded-lg text-white hover:bg-gray-700">
-                            <Gear size={32} />
-                            <span tw="ml-3">Configuração</span>
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-        </aside>
+        <>
+            <aside tw="md:flex hidden flex-col justify-between w-64 h-[100vh] bg-gray-800 rounded-r-xl">
+                <div tw="overflow-y-auto py-4 px-3 rounded-r bg-gray-800">
+                    <ul tw="space-y-2">
+                        <li>
+                            <NavLink to="/" tw="[&.active]:bg-gray-600 cursor-pointer flex items-center p-2 text-base font-normal  rounded-lg text-white hover:bg-gray-700">
+                                <ChartBar size={32} />
+                                <span tw="ml-3">Dashboard</span>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/admin" tw="[&.active]:bg-gray-600 cursor-pointer flex items-center p-2 text-base font-normal rounded-lg text-white hover:bg-gray-700">
+                                <Gear size={32} />
+                                <span tw="ml-3">Configuração</span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+            <footer tw="md:hidden fixed flex flex-row justify-around items-center left-0 bottom-0 w-full h-[7rem] bg-gray-800 ">
+                <NavLink to="/" tw="[&.active]:bg-gray-600 cursor-pointer flex items-center p-4 text-base font-normal  rounded-lg text-white hover:bg-gray-700">
+                    <ChartBar size={28} />
+                    <span tw="ml-3">Dashboard</span>
+                </NavLink>
+                <NavLink to="/admin" tw="[&.active]:bg-gray-600 cursor-pointer flex items-center p-4 text-base font-normal rounded-lg text-white hover:bg-gray-700">
+                    <Gear size={28} />
+                    <span tw="ml-3">Configuração</span>
+                </NavLink>
+            </footer>
+        </>
     )
 }
 
--- src/layout/defaultLayout/index.tsx
+++ src/layout/defaultLayout/index.tsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
 import NavBar from "../../components/navbar"
+import "twin.macro"
 
 const DefaultLayout: React.FC = () => {
     return (
-        <>
+        <div tw="flex">
             <NavBar />
             <Outlet />
-        </>
+        </div>
     )
 }
 
