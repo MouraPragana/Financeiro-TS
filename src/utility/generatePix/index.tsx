784611d29ac5d2ebb4358340d04b427f5ab8360c
Mateus Pragana <mateuspragana@gmail.com>
2022-11-14 18:47:17 -0300

    Criando função-utility geradora do pix.

---
+++ src/utility/generatePix/index.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { QrCodePix, QrCodePixParams } from "qrcode-pix";
+
+export function GeraPix({ params }: { params: QrCodePixParams }) {
+    const [base64, setbase64] = useState("");
+
+    useEffect(() => {
+        const qrCodePix = QrCodePix(params);
+        qrCodePix.base64().then(setbase64);
+    }, [params]);
+
+    return (
+        <div>
+            {base64 ? <img src={base64} alt="pix qr" /> : <div>pix-inválido</div>}
+        </div>
+    );
+}
