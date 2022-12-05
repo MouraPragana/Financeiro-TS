a6cd49f654fd7391c6bedf9c26e4b080268b48d1
Mateus Pragana <mateuspragana@gmail.com>
2022-12-05 20:17:13 -0300

    feat: Criar useLocalStorage hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorate = (key: string, defaultValue: string | string[]) => {
+  const [value, setValue] = useState(() => {
+    const currentValue = JSON.parse(
+      localStorage.getItem(key) || JSON.stringify(defaultValue)
+    );
+    return currentValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export { useLocalStorate };
