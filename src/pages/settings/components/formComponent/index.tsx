c7b4978c3dda1fe86cce72bc559834e7a80cee06
Mateus Pragana <mateuspragana@gmail.com>
2022-12-07 21:46:52 -0300

    refactor: Mover formulário para um componente próprio

---
+++ src/pages/settings/components/formComponent/index.tsx
@@ -0,0 +1,129 @@
+import {
+  Button,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { format, isValid } from "date-fns";
+import { useState } from "react";
+import { useFormContext } from "react-hook-form";
+import { GrCurrency } from "react-icons/gr";
+import "twin.macro";
+
+const FormComponent: React.FC = () => {
+  const { register } = useFormContext();
+
+  const [dataLancamento, setDataLancamento] = useState<Date>(new Date());
+  const dataDeLancamentoValida = isValid(dataLancamento);
+  const dataDeLancamentoString = format(dataLancamento, "dd/MM/yyyy");
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DesktopDatePicker
+          tw="w-full"
+          label="Data do lançamento"
+          inputFormat="dd/MM/yyyy"
+          value={dataLancamento}
+          onChange={(newValue) => {
+            newValue && setDataLancamento(newValue);
+          }}
+          renderInput={(params) => (
+            <TextField {...params} onKeyDown={onKeyDown} />
+          )}
+        />
+      </LocalizationProvider>
+
+      <TextField
+        tw="w-full"
+        id="outlined-basic"
+        label="Título do Lançamento"
+        variant="outlined"
+        autoComplete="off"
+        {...register("tituloLancamento")}
+      />
+
+      <TextField
+        tw="w-full"
+        id="outlined-basic"
+        label="Descrição do Lançamento"
+        variant="outlined"
+        autoComplete="off"
+        {...register("descricaoLancamento")}
+      />
+
+      <div tw="flex lg:flex-row flex-col lg:space-x-5 lg:space-y-0 space-y-5">
+        <FormControl tw="w-full">
+          <InputLabel id="tipo">Tipo</InputLabel>
+          <Select
+            labelId="tipo"
+            id="tipo"
+            value={"default"}
+            label="Tipo"
+            // onChange={handleChange}
+          >
+            <MenuItem value={"default"}>Selecione uma opção</MenuItem>
+            <MenuItem value={"ganho"}>Ganho</MenuItem>
+            <MenuItem value={"despesa"}>Despesa</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl tw="w-full">
+          <InputLabel id="classificacao">Classificação</InputLabel>
+          <Select
+            labelId="classificacao"
+            id="classificacao"
+            value={"default"}
+            label="Classificação"
+            // onChange={handleChange}
+          >
+            <MenuItem value={"default"}>Selecione uma opção</MenuItem>
+            <MenuItem value={"investimento"}>Investimento</MenuItem>
+            <MenuItem value={"salario"}>Salário</MenuItem>
+            <MenuItem value={"rendaExtra"}>Renda Extra</MenuItem>
+            <MenuItem value={"lazer"}>Lazer</MenuItem>
+            <MenuItem value={"transporte"}>Transporte</MenuItem>
+            <MenuItem value={"alimentacao"}>Alimentação</MenuItem>
+            <MenuItem value={"conta"}>Conta</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+      <TextField
+        tw="w-full"
+        id="outlined-basic"
+        label="Valor"
+        variant="outlined"
+        autoComplete="off"
+        {...register("valorLancamento", { valueAsNumber: true })}
+        type="number"
+        inputProps={{
+          step: 0.01,
+          min: 0,
+        }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <GrCurrency size={20} />
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <Button variant="contained" type="submit" sx={{ py: 1.45 }}>
+        Cadastrar lançamento
+      </Button>
+    </>
+  );
+};
+
+export { FormComponent };
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,29 +1,12 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Button,
-  FormControl,
-  InputAdornment,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from "@mui/material";
-import { DesktopDatePicker } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { format, isValid } from "date-fns";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import "twin.macro";
 import * as z from "zod";
-import { GrCurrency } from "react-icons/gr";
 import { useFinanceiroContext } from "../../context/financeiroContext";
+import { FormComponent } from "./components/formComponent";
 
 const Financial: React.FC = () => {
-  const [dataLancamento, setDataLancamento] = useState<Date>(new Date());
-  const dataDeLancamentoValida = isValid(dataLancamento);
-  const dataDeLancamentoString = format(dataLancamento, "dd/MM/yyyy");
-
   const { lancamentos, handleAddNewLancamento } = useFinanceiroContext();
 
   const schema = z.object({
@@ -42,11 +25,14 @@ const Financial: React.FC = () => {
     valorLancamento: number;
   }
 
-  const { register, handleSubmit } = useForm<IForm>({
+  const methods = useForm<IForm>({
     resolver: zodResolver(schema),
   });
 
+  const { handleSubmit } = methods;
+
   const onSubmit = (data: IForm) => {
+    console.log(data);
     // if (dataDeLancamentoValida) {
     //   console.log(data);
     //   console.log(dataDeLancamentoString);
@@ -61,13 +47,12 @@ const Financial: React.FC = () => {
     });
   };
 
-  const onKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
-    e.preventDefault();
+  const error = (e: any) => {
+    console.log(e);
   };
 
   return (
     <div tw="flex flex-col px-3 mx-5 py-4 w-[750px]">
-      {lancamentos[0].tituloLancamento}
       <h1 tw="md:text-2xl text-[1rem] font-bold mb-3 text-gray-900 text-justify">
         Lançamentos
       </h1>
@@ -77,102 +62,11 @@ const Financial: React.FC = () => {
       </p>
       <form
         tw="flex flex-col px-4 py-3 space-y-5 h-full w-full"
-        onSubmit={handleSubmit(onSubmit)}
+        onSubmit={handleSubmit(onSubmit, error)}
       >
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <DesktopDatePicker
-            tw="w-full"
-            label="Data do lançamento"
-            inputFormat="dd/MM/yyyy"
-            value={dataLancamento}
-            onChange={(newValue) => {
-              newValue && setDataLancamento(newValue);
-            }}
-            renderInput={(params) => (
-              <TextField {...params} onKeyDown={onKeyDown} />
-            )}
-          />
-        </LocalizationProvider>
-
-        <TextField
-          tw="w-full"
-          id="outlined-basic"
-          label="Título do Lançamento"
-          variant="outlined"
-          autoComplete="off"
-          {...register("tituloLancamento")}
-        />
-
-        <TextField
-          tw="w-full"
-          id="outlined-basic"
-          label="Descrição do Lançamento"
-          variant="outlined"
-          autoComplete="off"
-          {...register("descricaoLancamento")}
-        />
-
-        <div tw="flex lg:flex-row flex-col lg:space-x-5 lg:space-y-0 space-y-5">
-          <FormControl tw="w-full">
-            <InputLabel id="tipo">Tipo</InputLabel>
-            <Select
-              labelId="tipo"
-              id="tipo"
-              value={"default"}
-              label="Tipo"
-              // onChange={handleChange}
-            >
-              <MenuItem value={"default"}>Selecione uma opção</MenuItem>
-              <MenuItem value={"ganho"}>Ganho</MenuItem>
-              <MenuItem value={"despesa"}>Despesa</MenuItem>
-            </Select>
-          </FormControl>
-
-          <FormControl tw="w-full">
-            <InputLabel id="classificacao">Classificação</InputLabel>
-            <Select
-              labelId="classificacao"
-              id="classificacao"
-              value={"default"}
-              label="Classificação"
-              // onChange={handleChange}
-            >
-              <MenuItem value={"default"}>Selecione uma opção</MenuItem>
-              <MenuItem value={"investimento"}>Investimento</MenuItem>
-              <MenuItem value={"salario"}>Salário</MenuItem>
-              <MenuItem value={"rendaExtra"}>Renda Extra</MenuItem>
-              <MenuItem value={"lazer"}>Lazer</MenuItem>
-              <MenuItem value={"transporte"}>Transporte</MenuItem>
-              <MenuItem value={"alimentacao"}>Alimentação</MenuItem>
-              <MenuItem value={"conta"}>Conta</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-
-        <TextField
-          tw="w-full"
-          id="outlined-basic"
-          label="Valor"
-          variant="outlined"
-          autoComplete="off"
-          {...register("valorLancamento")}
-          type="number"
-          inputProps={{
-            step: 0.01,
-            min: 0,
-          }}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <GrCurrency size={20} />
-              </InputAdornment>
-            ),
-          }}
-        />
-
-        <Button variant="contained" type="submit" sx={{ py: 1.45 }}>
-          Cadastrar lançamento
-        </Button>
+        <FormProvider {...methods}>
+          <FormComponent />
+        </FormProvider>
       </form>
     </div>
   );
